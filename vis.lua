d6b4bd841f7fa6c25b6a57f07dd2e99c10879aa5
Christopher Moore <christopher.e.moore@gmail.com>
2026-05-08 10:36:42 +0700

    now flood-fills save per map

--- vis.lua
+++ vis.lua
@@ -8,6 +8,8 @@ local math = require 'ext.math'
 local assert = require 'ext.assert'
 local timer = require 'ext.timer'
 local number = require 'ext.number'
+local fromlua = require 'ext.fromlua'
+local tolua = require 'ext.tolua'
 local path = require 'ext.path'
 local vec2i = require 'vec-ffi.vec2i'
 local vec2d = require 'vec-ffi.vec2d'
@@ -22,6 +24,17 @@ local GLTex2D = require 'gl.tex2d'
 local GLGeometry = require 'gl.geometry'
 local GLSceneObject = require 'gl.sceneobject'
 local ig = require 'imgui'
+
+
+-- [[ alright this is unfortunately rigid in my vis design ... feel free to comment this out and just not do any flood-filling voxel-generation ...
+if path'../numo9':exists() then
+	package.path = package.path .. ';../numo9/?.lua'
+	Voxel = require 'numo9.rom'.Voxel
+	BlobVoxelMap = require 'numo9.blob.voxelmap'
+	BlobVoxelMap.skipGPU = true	-- we're not rendering, just saving
+end
+--]]
+
 
 local startTime = timer.getTime()
 
@@ -577,9 +590,6 @@ function MapWindow:setIndex(newIndex, pushStack)
 	end
 --]]
 
-	-- clear previous flood-fills
-	app.floodFillTiles = nil
-
 	-- start us off centered at the first door we find
 	if mapInfo.doors then
 		local e = mapInfo.doors[1]
@@ -619,7 +629,8 @@ function TileWindow:showIndexUI(ar)
 	ig.igText(x..', '..y..' of '..w..', '..h)
 
 	-- if getting x and y fail then mapInfo can't be accessed, so by here mapInfo is guaranteed
-	local mapInfo = game.getMap(self.app.mapWindow.index)
+	local mapIndex = self.app.mapWindow.index
+	local mapInfo = game.getMap(mapIndex)
 	local layerPos = mapInfo.layerPos
 	local layerSizes = mapInfo.layerSizes
 	local tilePropsData = mapInfo.tilePropsData
@@ -657,175 +668,186 @@ function TileWindow:showIndexUI(ar)
 	end
 
 
-	if mapInfo
-	and app.tilePropsTex
-	and app.tilePropsTex.data
-	then
-		local data = app.tilePropsTex.data
-		local mapWidth = app.tilePropsTex.width
-		local mapHeight = app.tilePropsTex.height
-		if ig.igButton'Flood Fill Tile...' then
-			local x = app.tileWindow.index % mapWidth
-			local y = (app.tileWindow.index - x) / mapWidth
+	if mapInfo then
+		local mapWidth, mapHeight = self.app.tileWindow:getMapSize()
 
-			-- now flood-fill against bit #17 (the fake one for the special-value of 'impassible')
-			local dirs = {
-				{1,0},
-				{0,1},
-				{-1,0},
-				{0,-1},
-			}
+		if app.tilePropsTex
+		and app.tilePropsTex.data
+		then
+			local data = app.tilePropsTex.data
+			if ig.igButton'Flood Fill Tile...' then
+				local x = app.tileWindow.index % mapWidth
+				local y = (app.tileWindow.index - x) / mapWidth
 
-			local layerPos = mapInfo.layerPos
-			local layerSizes = mapInfo.layerSizes
-			local layouts = mapInfo.layouts
-
-			local filled = {}
-			local fillBbox = box2i()
-			fillBbox.min:set(0x10000, 0x10000)	-- some max bound
-			fillBbox.max:set(-1, -1)				-- some min bound
-			local function writeFilled(x,y)
-				fillBbox:stretch(vec2i(x,y))
-				local i = x + mapWidth * y
-				-- read 16x16's here ...
-				local value = 0
-				for layer=1,3 do
-					local layout = layouts[layer]
-					local layerSize = layerSizes[layer]
-					local layoutData = layout and layout.data
-					if layoutData then
-
-						local posx, posy = 0, 0
-						if layerPos[layer]
-						-- if we have a position for the layer, but we're using parallax, then the position is going to be relative to the view
-						--and map.parallax == 0
-						then
-							posx, posy = layerPos[layer]:unpack()
-						end
-
-						local layoutptr = ffi.cast('uint8_t*', layoutData)
-						local srcX = (x - posx) % layerSize.x
-						local srcY = (y - posy) % layerSize.y
-						local tile16x16 = layoutptr[((srcX + layerSize.x * srcY) % #layoutData)]
-
-						value = bit.bor(value, bit.lshift(
-							tile16x16,
-							bit.lshift(layer-1, 3)	-- shift our 16x16 byte left 8 bits per layer...
-						))
+				local found
+				for _,ff in ipairs(app.floodFillTilesPerMap[mapIndex] or {}) do
+					if ff.filled[x + mapWidth * y] then
+						print'already flood-filled this region!'
+						found = true
 					end
 				end
+				if not found then
 
-				filled[i] = value
-			end
-			local function canTraverse(x,y)
-				return 0 == bit.band(mapTilePropsFlagForName.impassible, data[x + mapWidth * y])
-			end
-			if canTraverse(x,y) then
-				writeFilled(x, y)
-				local fillstack = table()
-				fillstack:insert{x, y}
-				while #fillstack > 0 do
-					local x0, y0 = table.unpack(fillstack:remove())
-					for _,dir in ipairs(dirs) do
-						local x1, y1 = x0 + dir[1], y0 + dir[2]
-						if x1 >= 0 and x1 < mapWidth
-						and y1 >= 0 and y1 < mapHeight
-						then
-							local i = x1 + mapWidth * y1
-							if not filled[i]
-							and canTraverse(x1, y1)
-							then
-								writeFilled(x1, y1)
-								fillstack:insert{x1, y1}
+					-- now flood-fill against bit #17 (the fake one for the special-value of 'impassible')
+					local dirs = {
+						{1,0},
+						{0,1},
+						{-1,0},
+						{0,-1},
+					}
+
+					local layerPos = mapInfo.layerPos
+					local layerSizes = mapInfo.layerSizes
+					local layouts = mapInfo.layouts
+
+					local filled = {}
+					local bbox = box2i()
+					bbox.min:set(0x10000, 0x10000)	-- some max bound
+					bbox.max:set(-1, -1)				-- some min bound
+					local function writeFilled(x,y)
+						bbox:stretch(vec2i(x,y))
+						local i = x + mapWidth * y
+						-- read 16x16's here ...
+						local value = 0
+						for layer=1,3 do
+							local layout = layouts[layer]
+							local layerSize = layerSizes[layer]
+							local layoutData = layout and layout.data
+							if layoutData then
+
+								local posx, posy = 0, 0
+								if layerPos[layer]
+								-- if we have a position for the layer, but we're using parallax, then the position is going to be relative to the view
+								--and map.parallax == 0
+								then
+									posx, posy = layerPos[layer]:unpack()
+								end
+
+								local layoutptr = ffi.cast('uint8_t*', layoutData)
+								local srcX = (x - posx) % layerSize.x
+								local srcY = (y - posy) % layerSize.y
+								local tile16x16 = layoutptr[((srcX + layerSize.x * srcY) % #layoutData)]
+
+								value = bit.bor(value, bit.lshift(
+									tile16x16,
+									bit.lshift(layer-1, 3)	-- shift our 16x16 byte left 8 bits per layer...
+								))
+							end
+						end
+
+						filled[i] = value
+					end
+					local function canTraverse(x,y)
+						return 0 == bit.band(mapTilePropsFlagForName.impassible, data[x + mapWidth * y])
+					end
+					if canTraverse(x,y) then
+						writeFilled(x, y)
+						local fillstack = table()
+						fillstack:insert{x, y}
+						while #fillstack > 0 do
+							local x0, y0 = table.unpack(fillstack:remove())
+							for _,dir in ipairs(dirs) do
+								local x1, y1 = x0 + dir[1], y0 + dir[2]
+								if x1 >= 0 and x1 < mapWidth
+								and y1 >= 0 and y1 < mapHeight
+								then
+									local i = x1 + mapWidth * y1
+									if not filled[i]
+									and canTraverse(x1, y1)
+									then
+										writeFilled(x1, y1)
+										fillstack:insert{x1, y1}
+									end
+								end
 							end
 						end
 					end
+
+					local hist = {}
+					for index, tileValue in pairs(filled) do
+						hist[tileValue] = (hist[tileValue] or 0) + 1
+					end
+					local tileValues = table.keys(hist):sort(function(a,b)
+						return hist[a] > hist[b]
+					end)
+					-- hmm TODO
+					-- histogram on layer1 tile or layer2 or on both?
+					local floorTile = bit.band(0xff, tileValues[1])
+					app.floodFillTilesPerMap[mapIndex] = app.floodFillTilesPerMap[mapIndex] or table()
+					app.floodFillTilesPerMap[mapIndex]:insert{
+						filled = filled,	-- key = tile index, value = int with each byte a layer's tile16x16 index
+						hist = hist,		-- key = int of tile16x16 layers (value of filled), value = occurrence count
+						tileValues = tileValues,	-- int of tile16x16 layers, sorted by occurrence
+						bbox = bbox,
+						floorTile = floorTile,
+						wallTile = 0,
+					}
 				end
 			end
-
-			local hist = {}
-			for index, tileValue in pairs(filled) do
-				hist[tileValue] = (hist[tileValue] or 0) + 1
-			end
-			local tileValues = table.keys(hist):sort(function(a,b)
-				return hist[a] > hist[b]
-			end)
-			-- hmm TODO
-			-- histogram on layer1 tile or layer2 or on both?
-			local floorTile = bit.band(0xff, tileValues[1])
-			app.floodFillTiles = {
-				filled = filled,	-- key = tile index, value = int with each byte a layer's tile16x16 index
-				hist = hist,		-- key = int of tile16x16 layers (value of filled), value = occurrence count
-				tileValues = tileValues,	-- int of tile16x16 layers, sorted by occurrence
-				bbox = fillBbox,
-				floorTile = floorTile,
-				wallTile = 0,
-			}
 		end
 
-		if app.floodFillTiles then
-			if ig.igButton'Clear Fill Tiles' then
-				app.floodFillTiles = nil
-			end
-		end
-
-		-- don't need to do this until you export but i'm testing it here so i dont have to jump thruogh all those hoops first to test it
-		if not BlobVoxelMap then
-			package.path = package.path .. ';../numo9/?.lua'
-			Voxel = require 'numo9.rom'.Voxel
-			BlobVoxelMap = require 'numo9.blob.voxelmap'
-			BlobVoxelMap.skipGPU = true	-- we're not rendering, just saving
-		end
-
-
-		local floodFillTiles = app.floodFillTiles
-		if floodFillTiles then
-			ig.igText'flood fill bounds:'
-			local bbox = floodFillTiles.bbox
-			ig.luatableInputInt('flood fill min x', bbox.min, 'x')
-			ig.luatableInputInt('flood fill min y', bbox.min, 'y')
-			ig.luatableInputInt('flood fill max x', bbox.max, 'x')
-			ig.luatableInputInt('flood fill max y', bbox.max, 'y')
-
-			-- AsText so it can handle all lua number parsing, including 0x's
-			ig.luatableInputFloatAsText('floor tile', floodFillTiles, 'floorTile')
-			ig.luatableInputFloatAsText('wall tile', floodFillTiles, 'wallTile')
-
-			if ig.igButton'export voxelmap' then
-				-- 1) make our voxelmap
-				local vector = require 'stl.vector-lua'
-				local size = vec3i(bbox.max.x - bbox.min.x, bbox.max.y - bbox.min.y, 8)
-				local v = vector(Voxel, 3 + size:volume())
-				ffi.cast('vec3i*', v.v)[0] = size
-				for i=3,3+size:volume()-1 do
-					v.v[i].intval = 0xffffffff
+		local floodFillTilesForThisMap = app.floodFillTilesPerMap[mapIndex]
+		if floodFillTilesForThisMap then
+			for ffIndex,floodFillTiles in ipairs(floodFillTilesForThisMap) do
+				ig.igSeparator()
+				if ig.igButton'Clear Fill Tiles' then
+					floodFillTilesForThisMap:remove(ffIndex)
+					return	-- iterated table is invalidated
 				end
-				local voxelmap = BlobVoxelMap(v:dataToStr())
-				-- 2) copy ground tiles across
-				for y=0,tonumber(size.y)-1 do
-					for x=0,tonumber(size.x)-1 do
-						local vox = v.v + (3 + x + size.x * y)
-						vox.spriteIndex = floodFillTiles.floorTile
-						vox.mesh3DIndex = 0	-- hmm todo, floor voxel mesh index
 
-						-- if this is a non-traversible tile then grow the walls
-						local i = x + bbox.min.x + mapWidth * (y + bbox.min.y)
-						if not floodFillTiles.filled[i] then
-							for z=1,size.z-1 do
-								local vox = v.v + (3 + x + size.x * (y + size.y * z))
-								vox.spriteIndex = floodFillTiles.wallTile	-- TODO make this the most-prominent-wall-tile
-								-- idk how to find that out ... trace from top of traversible filled region to the ceiling tile and count up most-prevalent
-								vox.mesh3DIndex = 0	-- hmm todo, wall voxel mesh index
+				ig.igText'flood fill bounds:'
+				local bbox = floodFillTiles.bbox
+				ig.luatableInputInt('flood fill min x', bbox.min, 'x')
+				ig.luatableInputInt('flood fill min y', bbox.min, 'y')
+				ig.luatableInputInt('flood fill max x', bbox.max, 'x')
+				ig.luatableInputInt('flood fill max y', bbox.max, 'y')
+
+				-- AsText so it can handle all lua number parsing, including 0x's
+				ig.luatableInputFloatAsText('floor tile', floodFillTiles, 'floorTile')
+				ig.luatableInputFloatAsText('wall tile', floodFillTiles, 'wallTile')
+
+				if ig.igButton'export voxelmap' then
+					-- 1) make our voxelmap
+					local vector = require 'stl.vector-lua'
+					local size = vec3i(bbox.max.x - bbox.min.x, bbox.max.y - bbox.min.y, 8)
+
+					local voxelmap
+					do
+						local v = vector(Voxel, 3 + size:volume())
+						ffi.cast('vec3i*', v.v)[0] = size
+						for i=3,3+size:volume()-1 do
+							v.v[i].intval = 0xffffffff
+						end
+						voxelmap = BlobVoxelMap(v:dataToStr())
+					end
+					-- 2) copy ground tiles across
+					for y=0,tonumber(size.y)-1 do
+						for x=0,tonumber(size.x)-1 do
+							local vox = voxelmap:getVoxelBlobPtr(x, y, 0)
+							vox.spriteIndex = floodFillTiles.floorTile
+							vox.mesh3DIndex = 0	-- hmm todo, floor voxel mesh index
+
+							-- if this is a non-traversible tile then grow the walls
+							local i = x + bbox.min.x + mapWidth * (y + bbox.min.y)
+							if not floodFillTiles.filled[i] then
+								for z=1,size.z-1 do
+									local vox = voxelmap:getVoxelBlobPtr(x, y, z)
+									-- TODO if it's a north wall then copy source tiles going y+
+									-- and if it's any other wall then copy from the north tile wall profile
+									vox.spriteIndex = floodFillTiles.wallTile	-- TODO make this the most-prominent-wall-tile
+									-- idk how to find that out ... trace from top of traversible filled region to the ceiling tile and count up most-prevalent
+									vox.mesh3DIndex = 0	-- hmm todo, wall voxel mesh index
+								end
 							end
 						end
 					end
+					path('exported-voxelmap-map'..mapIndex..'.vox'):write(voxelmap:toBinStr())
 				end
-				path('exported-voxelmap-map'..self.app.mapWindow.index..'.vox'):write(voxelmap:toBinStr())
-			end
 
-			ig.igText('flood fill histogram:')
-			for _,tileValue in ipairs(floodFillTiles.tileValues) do
-				ig.igText('\t'..('0x%06x'):format(tileValue)..' = '..floodFillTiles.hist[tileValue])
+				ig.igText('flood fill histogram:')
+				for _,tileValue in ipairs(floodFillTiles.tileValues) do
+					ig.igText('\t'..('0x%06x'):format(tileValue)..' = '..floodFillTiles.hist[tileValue])
+				end
 			end
 		end
 	end
@@ -1330,6 +1352,8 @@ local App = require 'imgui.appwithorbit'()
 App.hasFocus = true
 App.title = 'FF6 Data Visualizer'
 
+local floodFillSavePath = path'floodfill-save.lua'
+
 function App:initGL(...)
 	App.super.initGL(self, ...)
 
@@ -1530,11 +1554,46 @@ void main() {
 	self.mapSize = vec2d()
 
 
+	self.floodFillTilesPerMap = table()
+	if floodFillSavePath:exists() then
+		self.floodFillTilesPerMap = table(
+			(assert(fromlua(
+				(assert(
+					floodFillSavePath:read()
+				)),
+				nil, nil, {
+					box2i = box2i,
+				}
+			)))
+		)
+	end
+
 	if cmdline[1] then
 		self:onLoadROM(cmdline[1])
 	end
 end
 
+function App:exit(...)
+
+	assert(floodFillSavePath:write(
+		(assert(tolua(
+			self.floodFillTilesPerMap,
+			{
+				serializeForType = {
+					cdata = function(state, x, tab, luapath, keyRef)
+						if box2i:isa(x) then
+							return 'box2i'..tostring(x)
+						else
+							error("tolua got unknown cdata "..tostring(x))
+						end
+					end,
+				},
+			}
+		)))
+	))
+
+	App.super.exit(self, ...)
+end
 
 function App:onLoadROM(infn)
 	-- muh globals
@@ -1985,18 +2044,22 @@ function App:update()
 
 
 				-- tempting to implement this as a bitflag layer ...
-				if self.floodFillTiles then
-					for i in pairs(self.floodFillTiles.filled) do
-						local x = i % mapWidth
-						local y = (i - x) / mapWidth
-						settable(uniforms.bbox, x, y, 1, 1)
-						settable(uniforms.color, 1,.5,0,1)
-						rectObj:draw()
+				local mapIndex = self.mapWindow.index
+				local floodFillTilesForThisMap = app.floodFillTilesPerMap[mapIndex]
+				if floodFillTilesForThisMap then
+					for ffIndex,floodFillTiles in ipairs(floodFillTilesForThisMap) do
+						for i in pairs(floodFillTiles.filled) do
+							local x = i % mapWidth
+							local y = (i - x) / mapWidth
+							settable(uniforms.bbox, x, y, 1, 1)
+							settable(uniforms.color, 1,.5,0,1)
+							rectObj:draw()
+						end
+						local bbox = floodFillTiles.bbox
+						settable(uniforms.bbox, bbox.min.x, bbox.min.y, bbox.max.x - bbox.min.x + 1, bbox.max.y - bbox.min.y + 1)
+						settable(uniforms.color, 1,1,1,1)
+						showHL()
 					end
-					local bbox = self.floodFillTiles.bbox
-					settable(uniforms.bbox, bbox.min.x, bbox.min.y, bbox.max.x - bbox.min.x + 1, bbox.max.y - bbox.min.y + 1)
-					settable(uniforms.color, 1,1,1,1)
-					showHL()
 				end
 			end
 
